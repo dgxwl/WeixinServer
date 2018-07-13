817fefd40410a37163900bf97d39ab0c3d458b78
wlxie <123456>
2018-07-13 10:24:27 +0800

    update .gitignore

--- src/main/java/cn/weixin/controller/CustomizeMenuController.java
+++ src/main/java/cn/weixin/controller/CustomizeMenuController.java
@@ -2,25 +2,23 @@ package cn.weixin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import cn.weixin.entity.customizemenu.Menu;
 import cn.weixin.service.iservice.ICustomizeMenuService;
 
-@Controller
+@RestController
 @RequestMapping("/customizemenu")
 public class CustomizeMenuController {
 	
 	@Autowired
-	@Qualifier("customizeService")
-	private ICustomizeMenuService customizeService;
+	@Qualifier("customizeMenuService")
+	private ICustomizeMenuService customizeMenuService;
 	
 	@RequestMapping("/createmenu")
-	@ResponseBody
 	public String createMenu(Menu menu, String appid, String appsecret) {  //TODO 临时定义一个返回值, 以后根据设计修改
-		int result = customizeService.createMenu(menu, appid, appsecret);
+		int result = customizeMenuService.createMenu(menu, appid, appsecret);
 		if (result == 0) {
 			//创建成功
 			return "创建成功";
@@ -31,9 +29,8 @@ public class CustomizeMenuController {
 	}
 	
 	@RequestMapping("/deletemenu")
-	@ResponseBody
 	public String deleteMenu(String appid, String appsecret) {    //TODO 同上
-		int result = customizeService.deleteMenu(appid, appsecret);
+		int result = customizeMenuService.deleteMenu(appid, appsecret);
 		if (result == 0) {
 			//删除成功
 			return "删除成功";
--- src/main/java/cn/weixin/controller/MainController.java
+++ src/main/java/cn/weixin/controller/MainController.java
@@ -2,17 +2,16 @@ package cn.weixin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import cn.weixin.service.iservice.IMainService;
 import cn.weixin.utils.SignUtil;
 
 @RequestMapping("/main")
-@Controller
+@RestController
 public class MainController {
 	
 	@Autowired
@@ -20,13 +19,11 @@ public class MainController {
 	private IMainService mainService;
 
 	@RequestMapping("/test")
-	@ResponseBody
 	public String test() {
 		return "hello world";
 	}
 	
 	@RequestMapping(value="/core", method=RequestMethod.GET)
-	@ResponseBody
 	public String checkSignature(String signature, String timestamp, String nonce, String echostr) {
 		if (SignUtil.checkSignature(signature, timestamp, nonce)) {
             return echostr;
@@ -35,8 +32,18 @@ public class MainController {
 	}
 	
 	@RequestMapping(value="/core", method=RequestMethod.POST)
-	@ResponseBody
 	public String core(@RequestBody String xmlStr) {
 		return mainService.processRequest(xmlStr);
+	}
+	
+	@RequestMapping("/haha")
+	public String haha(int[] i) {
+		char[] c = new char[i.length];
+		StringBuilder s = new StringBuilder();
+		for (int j = 0; j < i.length; j++) {
+			c[j] = (char) i[j];
+			s.append(c[j]);
+		}
+		return s.toString();
 	}
 }
